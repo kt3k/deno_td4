7268d6c294b34dd5efd6b07bd1800c7a05209ccd
Yoshiya Hinosawa <stibium121@gmail.com>
2019-06-22 00:26:29 +0900

    add tests

--- test.ts
+++ test.ts
@@ -1,23 +1,32 @@
 import { runIfMain, assertEquals, test } from "./dev_deps.ts";
 import { CPUState } from "./mod.ts";
 
-test(function testOutIm() {
-  const cpu = new CPUState([0xb1]); // OUT_IM 1
+test(function test0() {
+  // OUT_IM 1
+  const cpu = new CPUState([0xb1]);
   cpu.run();
-  console.log(cpu);
   assertEquals(cpu.io.out, [1]);
 });
 
 test(function test1() {
-  // ADD A 1
-  // ADD A 2
-  // ADD A 3
-  // MOV B A
-  // OUT B
+  // ADD_A 1
+  // ADD_A 2
+  // ADD_A 3
+  // MOV_A2B
+  // OUT_B
   const cpu = new CPUState([0x01, 0x02, 0x03, 0x40, 0x90]);
   cpu.run();
-  console.log(cpu);
-  assertEquals(cpu.io.out, [6]);
+  assertEquals(cpu.io.out, [0x6]);
+});
+
+test(function test2() {
+  // ADD_B 2
+  // ADD_B 3
+  // ADD_B 4
+  // OUT_B
+  const cpu = new CPUState([0x52, 0x53, 0x54, 0x90]);
+  cpu.run();
+  assertEquals(cpu.io.out, [0x9]);
 });
 
 runIfMain(import.meta);
